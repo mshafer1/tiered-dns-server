544100dbffa6c050551fe136703abaadbba9e9c7
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2026-07-18 15:07:25 -0500

    it's already moved

--- src/ubuntu__configure_system_dns.sh
+++ src/ubuntu__configure_system_dns.sh
@@ -5,8 +5,6 @@ sed -e 's/#?DNSStubListener=.*/DNSStubListener=no/' -i /etc/systemd/resolved.con
 
 mv /etc/resolv.conf /etc/resolv.conf.bak
 
-rm /etc/resolv.conf
-
 cat <<EOF > /etc/resolv.conf
 nameserver 1.1.1.3
 nameserver 208.67.222.222
