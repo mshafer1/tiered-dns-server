52a22ba33ce5552150ef796ed5b34fff52a26549
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2026-07-18 09:54:41 -0500

    try to make sure cron is running

--- src/setup_updates.sh
+++ src/setup_updates.sh
@@ -3,3 +3,8 @@ set -euo pipefail
 
 mkdir -p /etc/cron.d
 echo "0 3 * * 6 root bash $(dirname "$0")/update-all.sh >> /var/log/update-all.log 2>&1" > /etc/cron.d/weekly-updates
+
+# make sure cron is running
+systemctl enable cron
+systemctl start cron
+service cron restart
