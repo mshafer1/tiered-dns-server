de5d297e18aab4d85156d4be2aeca16a0320fd94
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2026-07-19 21:47:33 -0500

    use realpath instead of cd

--- src/update-all.sh
+++ src/update-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
 # doing repo/docker first so that no work is needed after reboot
 for file in self system; do
